5fc92018f21691629b16559b2f63de21c06559cb
Johnny Li <johnny@ticketfi.com>
2020-04-07 13:31:00 -0400

    Updates

---
+++ precache-manifest.6c4e2a0851d94ef7eba8482e46657550.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae20a68fb2faf405cdf50dab612319f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e046b66de71cd699134b",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "3a35d594817f7b85e76c",
+    "url": "/static/css/main.a3c62bd5.chunk.css"
+  },
+  {
+    "revision": "e046b66de71cd699134b",
+    "url": "/static/js/2.dadba1a2.chunk.js"
+  },
+  {
+    "revision": "bca055fc4dbca9dd1aa14293a6bc338f",
+    "url": "/static/js/2.dadba1a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a35d594817f7b85e76c",
+    "url": "/static/js/main.8bf76603.chunk.js"
+  },
+  {
+    "revision": "74b441b77d8c869ff9cf",
+    "url": "/static/js/runtime-main.42d2e5d0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
